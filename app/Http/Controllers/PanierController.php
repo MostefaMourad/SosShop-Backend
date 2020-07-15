24c918f83e9023959fff786898886df850fc6542
MostefaMourad <hm_mostefa@esi.dz>
2020-07-15 15:08:20 +0100

    Panier APIs implementation

--- app/Http/Controllers/PanierController.php
+++ app/Http/Controllers/PanierController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\APIHelpers;
+use App\Panier;
 use Illuminate\Http\Request;
 
 class PanierController extends Controller
 {
-    //
+    public function index()
+    {
+        $paniers = Panier::all();
+        $response = APIHelpers::createAPIResponse(false, 200, '', $paniers);
+        return response()->json($response, 200);
+    }
+
+    public function show($id)
+    {
+        $panier = Panier::find($id);
+        if ($panier == null) {
+            $response = APIHelpers::createAPIResponse(true, 204, 'Panier introuvable', null);
+        } else {
+            $response = APIHelpers::createAPIResponse(false, 200, 'Panier disponible', $panier);
+        }
+        return response()->json($response, 200);
+    }
+
+    public function destroy($id)
+    {
+        $panier = Panier::find($id);
+        if ($panier == null) {
+            $response = APIHelpers::createAPIResponse(true, 400, 'echec panier Introuvable', null);
+            return response()->json($response, 400);
+        } else {
+            $panier_delete = $panier->delete();
+            if ($panier_delete) {
+                $response = APIHelpers::createAPIResponse(false, 200, 'Suppression avec succes', $panier);
+                return response()->json($response, 200);
+            } else {
+                $response = APIHelpers::createAPIResponse(true, 400, 'echec', null);
+                return response()->json($response, 400);
+            }
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('panier')->group(function () {
+    Route::get('/', 'PanierController@index');
+    Route::get('/show/{id}', 'PanierController@show');
+    Route::delete('/delete/{id}', 'PanierController@destroy');
+ });
