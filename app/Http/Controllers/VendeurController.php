d7dc72139ba54e9cd967ff48a9a1a0e2f2ae47e1
MostefaMourad <hm_mostefa@esi.dz>
2020-07-23 02:31:25 +0100

    Implementing Vendeurr CRUD APIs

--- app/Http/Controllers/VendeurController.php
+++ app/Http/Controllers/VendeurController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\APIHelpers;
+use App\Vendeur;
 use Illuminate\Http\Request;
 
 class VendeurController extends Controller
 {
-    //
+    public function index()
+    {
+        $vendeurs = Vendeur::all();
+        $response = APIHelpers::createAPIResponse(false, 200, '', $vendeurs);
+        return response()->json($response, 200);
+    }
+
+
+
+    public function show($id)
+    {
+        $vendeur = Vendeur::find($id);
+        if ($vendeur == null) {
+            $response = APIHelpers::createAPIResponse(true, 204, 'vendeur introuvable', null);
+        } else {
+            $response = APIHelpers::createAPIResponse(false, 200, 'vendeur disponible', $vendeur);
+        }
+        return response()->json($response, 200);
+    }
+
+    
+    public function destroy($id)
+    {
+        $vendeur = Vendeur::find($id);
+        if ($vendeur == null) {
+            $response = APIHelpers::createAPIResponse(true, 400, 'echec vendeur Introuvable', null);
+            return response()->json($response, 400);
+        } else {
+            $vendeur_delete = $vendeur->delete();
+            if ($vendeur_delete) {
+                $response = APIHelpers::createAPIResponse(false, 200, 'Suppression avec succes', $vendeur);
+                return response()->json($response, 200);
+            } else {
+                $response = APIHelpers::createAPIResponse(true, 400, 'echec', null);
+                return response()->json($response, 400);
+            }
+        }
+    }   
 }
